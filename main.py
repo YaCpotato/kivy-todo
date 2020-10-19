20e60916a7b3cff072d8b46102bed83671cd6558
YaCpotato <yasshisshy@gmail.com>
2020-10-19 22:53:10 +0900

    サンプルチェック

--- main.py
+++ main.py
@@ -2,22 +2,18 @@ from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
+from kivy.lang import Builder       
 
-class List(BoxLayout):
-    def __init__(self, **kwargs):
-        super(List, self).__init__(**kwargs)
+class MainApp(App):
+    def build(self):
+        Builder.load_file('./main.kv')
+        layout = FloatLayout()
         self.rv.data = []
         btn_list = ['ひつまぶし','味噌煮込みうどん','味噌カツ','台湾ラーメン' \
                     ,'手羽先','小倉トースト','きしめん','あんかけスパ','どて煮' \
                     ,'ういろう','甘口バナナスパ']
         for btn_list_any in btn_list:
             self.rv.data.append({'value': btn_list_any})
-
-
-class MainApp(App):
-    def build(self):
-        layout = FloatLayout()
-        todo_list = List()
         ti = TextInput(text='Hello world', multiline=False)
         layout.add_widget(ti)
         layout.add_widget(todo_list)
--- sample.py
+++ sample.py
@@ -1,64 +1,29 @@
-from random import sample
-from string import ascii_lowercase
+import kivy
+kivy.require('1.0.5')
+
+from kivy.uix.floatlayout import FloatLayout
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-from kivy.core.text import LabelBase, DEFAULT_FONT
-from kivy.resources import resource_add_path
-# 日本語フォント設定
-resource_add_path('./fonts')
-kv = """
-<VariousButtons>:
-    canvas:
-        Color:
-            rgba: 1, 1, 1, 1
-        Rectangle:
-            size: self.size
-            pos: self.pos
-    value: ''
-    Button:
-        text: root.value
-        background_normal: ''
-        background_color: 0.5, 0.5, 0.75, 1
-        color: 1, 1 ,1 ,1
-        on_press: root.on_select_button(self)
-<Test>:
-    canvas:
-        Color:
-            rgba: 0.3, 0.3, 0.3, 1
-        Rectangle:
-            size: self.size
-            pos: self.pos
-    rv: rv
-    RecycleView:
-        id: rv
-        scroll_type: ['bars', 'content']
-        scroll_wheel_distance: sp(60) #スクロール速度
-        bar_width: sp(20)
-        viewclass: 'VariousButtons'
-        RecycleBoxLayout:
-            default_size: None, sp(160)
-            default_size_hint: 1, None
-            size_hint_y: None
-            height: self.minimum_height
-            orientation: 'vertical'
-            spacing: dp(8)
-"""
-Builder.load_string(kv)
-class Test(BoxLayout):
-    def __init__(self, **kwargs):
-        super(Test, self).__init__(**kwargs)
-        self.rv.data = []
-        btn_list = ['ひつまぶし','味噌煮込みうどん','味噌カツ','台湾ラーメン' \
-                    ,'手羽先','小倉トースト','きしめん','あんかけスパ','どて煮' \
-                    ,'ういろう','甘口バナナスパ']
-        for btn_list_any in btn_list:
-            self.rv.data.append({'value': btn_list_any})
-class VariousButtons(BoxLayout):
-    def on_select_button(self, button):
-        print('press:'+button.text)
-class TestApp(App):
+from kivy.properties import ObjectProperty, StringProperty
+
+
+class Controller(FloatLayout):
+    '''Create a controller that receives a custom widget from the kv lang file.
+
+    Add an action to be called from the kv lang file.
+    '''
+    label_wid = ObjectProperty()
+    info = StringProperty()
+
+    def do_action(self):
+        self.label_wid.text = 'My label after button press'
+        self.info = 'New info text'
+
+
+class ControllerApp(App):
+
     def build(self):
-        return Test()
+        return Controller(info='Hello world')
+
+
 if __name__ == '__main__':
-    TestApp().run()
+    ControllerApp().run()
